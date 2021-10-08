244135ca5ad825d29ac16b467235f9eb831cec17
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-07 17:45:11 -0700

    initial alter.sql commit

---
+++ alter-table.sql
@@ -0,0 +1,12 @@
+-- ****Intermediate - Alter Table****
+-- 1. Copy, paste, and run the insert statement from the animal section in delete.sql. 
+
+-- 2. Add a column to the animals table called “location”. It should have a VARCHAR data type.
+-- 3. Insert 3 new animals into the table – make sure none of their fields are null.
+-- 4. Change the “type” column’s name to be “species”.
+-- 5. Change the “species” column data type to be VARCHAR.
+
+-- ****Intermediate - Group By****
+-- 1. Find a count of how many tracks there are per genre. Display the genre name with the count.
+-- 2. Find a count of how many tracks are the ***“Pop”*** genre and how many tracks are the ***“Rock”*** genre.
+-- 3. Find a list of all artists and how many albums they have.
