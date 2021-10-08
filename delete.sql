d8898999e0cc36b172872540df25ef4962aa5c45
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-07 17:20:35 -0700

    initial delete.sql commit

---
+++ delete.sql
@@ -0,0 +1,20 @@
+-- ### **Delete**
+
+-- **Deleting is DANGEROUS!**
+
+-- Always do a select before a delete to make sure you get back exactly what you want and only what you want to delete!
+
+-- Since we cannot delete anything from the pre-defined tables ( foreign key restraints ), use the following SQL code to create a dummy table:
+
+-- **CREATE TABLE animals (**id SERIAL PRIMARY KEY, name VARCHAR(50), type TEXT, age INTEGER
+
+-- );
+
+-- INSERT INTO animals ( name, type, age ) VALUES (‘Leo’, ‘lion’, 12), (‘Jerry’, ‘mouse’, 4), (‘Marty’, ‘zebra’, 10), (‘Gloria’, ‘hippo’, 8), (‘Alex’, ‘lion’, 9), (‘Melman’, ‘giraffe’, 15), (‘Nala’, ‘lion’, 2), (‘Marie’, ‘cat’, 1), (‘Flounder’, ‘fish’, 8);
+
+-- SELECT * FROM animals;
+
+-- 1. Copy, paste, and run the SQL code from the summary.
+-- 2. Delete all ***‘lion’*** entries from the table.
+-- 3. Delete all animals whose names start with “M”.
+-- 4. Delete all entries whose age is less than 9.
